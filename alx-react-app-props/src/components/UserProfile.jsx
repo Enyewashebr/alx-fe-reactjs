e0d4ba4db3a14963231c3a26cf17f424807ec06b
Enyewashebr <Enyewashebir0@gmail.com>
2024-08-18 23:52:45 +0300

    final

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import UserContext from "./UserContext";
+
 
 const UserProfile = (props) => {
+  const userData = useContext(UserContext);
   return (
     <div>
       <h2>{props.name}</h2>
