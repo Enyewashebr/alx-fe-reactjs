e295b5996b20fd49774ce0749a439eaa1fd25931
Enyewashebr <Enyewashebir0@gmail.com>
2024-08-23 17:16:42 +0300

    Enhancing the Recipe Sharing Application

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,12 +1,15 @@
 
 import { useRecipeStore } from "../recipeStore";
 import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
 
 const DeleteRecipeButton = ({ recipeId }) => {
+    const navigate = useNavigate();
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
 
   const handleDelete = () => {
     deleteRecipe(recipeId);
+     navigate("/recipes");
   };
 
   return <button onClick={handleDelete}>Delete Recipe</button>;
