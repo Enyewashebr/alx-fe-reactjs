6fc7bee63978b9fff586ac841f17fb1a83cc5a03
Enyewashebr <Enyewashebir0@gmail.com>
2024-08-23 16:14:58 +0300

    Zustand

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,7 +1,7 @@
 
-import create from "zustand";
+import {create} from "zustand";
 
-const useRecipeStore = create((set) => ({
+const useRecipeStore = create(set => ({
   recipes: [],
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
