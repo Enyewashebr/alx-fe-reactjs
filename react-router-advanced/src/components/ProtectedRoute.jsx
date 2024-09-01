016ab229025972b0928acc75588c2f3c3608b46e
Enyewashebr <Enyewashebir0@gmail.com>
2024-09-01 17:15:22 +0300

    Done

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,6 +1,14 @@
 
 import { Navigate } from "react-router-dom";
 import React from "react";
+import useAuth from "../hooks/useAuth";
+
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated } = useAuth();
+
+  return isAuthenticated ? <>{children}</> : <Navigate to="/" />;
+};
 
 
 const ProtectedRoute = ({ children, isAuthenticated }) => {
