6183cd19be8c1c04a4bcd514c4c4a2457757bf0d
Enyewashebr <Enyewashebir0@gmail.com>
2024-09-01 21:51:59 +0300

    Modified

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
-import TodoList from "../../TodoList";
+import TodoList from "../TodoList";
 //import { act } from "react-dom/test-utils";
 import { act } from "react";
 import "@testing-library/jest-dom";
