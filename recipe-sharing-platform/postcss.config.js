515951b72485d4cd211cb8cab7e272ad54a07429
Enyewashebr <Enyewashebir0@gmail.com>
2024-09-15 13:45:57 +0300

    modified

--- recipe-sharing-platform/postcss.config.js
+++ recipe-sharing-platform/postcss.config.js
@@ -1,6 +1,8 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
+  theme: {
+    extend: {},
   },
-}
+  plugins: [],
+};
