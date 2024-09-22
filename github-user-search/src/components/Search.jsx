b5ab4fafdc40a1c4a2c7289adc13a4c191e8a4b1
Enyewashebr <Enyewashebir0@gmail.com>
2024-09-22 22:24:10 +0300

    Done

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function Search({ onSubmit }) {
+function Search({ onSubmit, users, loading, error }) {
   const [username, setUsername] = useState("");
   const [location, setLocation] = useState("");
   const [minRepos, setMinRepos] = useState(0);
@@ -11,27 +11,44 @@ function Search({ onSubmit }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="GitHub username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Location"
-        value={location}
-        onChange={(e) => setLocation(e.target.value)}
-      />
-      <input
-        type="number"
-        placeholder="Minimum Repositories"
-        value={minRepos}
-        onChange={(e) => setMinRepos(e.target.value)}
-      />
-      <button type="submit">Search</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="GitHub username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Location"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Minimum Repositories"
+          value={minRepos}
+          onChange={(e) => setMinRepos(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      {loading && <p>Loading...</p>}
+
+      {error && <p>Looks like we can't find the user. Please try again.</p>}
+
+      {users && users.length > 0 && (
+        <ul>
+          {users.map((user) => (
+            <li key={user.login}>
+              <img src={user.avatar_url} alt={user.login} />
+              <p>{user.login}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 }
 
