a6b5dd3b073a74eccf2f755a699f3755f2a243a3
Enyewashebr <Enyewashebir0@gmail.com>
2024-09-22 12:43:38 +0300

    initial commit

---
+++ github-user-search/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Home from "./components/Home";
+// import UserProfile from "./components/UserProfile";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <h1>GitHub User Search</h1>
+        {/* <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/user/:username" component={UserProfile} />
+        </Switch> */}
+      </div>
+    </Router>
+  );
+}
+
+export default App;
